169b5061d6f8aaf964a2a4a2717d3f1b03febc41
maqsadjon2024 <maqsadjonqurbondurdiev58@gmail.com>
2026-02-28 17:38:31 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+#1
+class Talaba:
+    ism = "Ali"
+    yosh = 18
+    kurs = 1
+
+t = Talaba()
+print(t.ism, t.yosh, t.kurs)
+
+#2
+class Telefon:
+    brend = "Samsung"
+    model = "A15"
+    narx = 200
+
+a = Telefon()
+print(a.brend, a.model, a.narx)
+
+#3
+class Kompyuter:
+    protsessor = "i5"
+    ram = 8
+    ssd = 256
+
+b = Kompyuter()
+print(b.protsessor, b.ram, b.ssd)
+
+#4
+class Kitob:
+    nom = "Python"
+    muallif = "Noma'lum"
+    yil = 2024
+
+d = Kitob()
+print(d.nom, d.muallif, d. yil)
+
+#5
+class Avtomobil:
+    model = "Cobalt"
+    rang = "Oq"
+    yil = 2023
+    
+s = Avtomobil()
+print(s.model, s.rang, s.yil)
